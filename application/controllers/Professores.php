c89643fafcbf48b14f5aaea796cdabb5006921b2
Pedro Hugo <47755506+PedroHugo11@users.noreply.github.com>
2019-10-23 10:30:30 -0300

    Consertando inicio professores.php

--- application/controllers/Professores.php
+++ application/controllers/Professores.php
@@ -21,6 +21,7 @@ class Professores extends MY_Controller {
 
 			$this->load->view('templates/header');
 			$this->load->view('templates/menu');
+			$this->load->view('templates/inicio');
 			$this->load->view('professores/index', $data);
 			$this->load->view('templates/footer');
 		} else {
