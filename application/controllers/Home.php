3aa10c40cb59cd4b328b0beb544fb1c839548ba4
Pedro Hugo <47755506+PedroHugo11@users.noreply.github.com>
2019-10-23 10:42:18 -0300

    aumentando quantidade de noticias home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,7 +9,7 @@ class Home extends MY_Controller {
 	}
 
 	public function index() {
-		$data['noticias'] = $this->noticias_anpae_model->get(array(), 5);
+		$data['noticias'] = $this->noticias_anpae_model->get(array(), 6);
 		$data['informativos'] = $this->informativos_model->get(array(), 5);
 		$this->load->view('templates/header');
 
