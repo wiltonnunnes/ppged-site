5c7e1a2ae0c940598c9967301e43c945e8d3496a
Pedro Hugo <47755506+PedroHugo11@users.noreply.github.com>
2019-10-23 10:17:06 -0300

    Atualizando sideBar.php

--- application/views/templates/sideBar.php
+++ application/views/templates/sideBar.php
@@ -1,12 +1,13 @@
-<script src="js/sideBar.js"></script>
+<script src="<?php echo base_url('js/sideBar.js'); ?>"></script>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
 		<div class="content">
 			<div id="jquery-accordion-menu" class="jquery-accordion-menu">
 				<div class="jquery-accordion-menu-header">Menu</div>
 				<ul>
-					<li><a href="#"><i class="fa fa-home"></i>O Programa</a>
-
+					<li><a href="#"><i class="fa fa-home"></i>Início</a></li>
+					<li>
+						<a href="#"><i class="fa fa-cog"></i>O Programa</a>
 						<ul class="submenu">
 							<li><a href="<?php echo site_url('apresentacao'); ?>">apresentacao</a></li>
 							<li><a href="<?php echo site_url('professores'); ?>">Docentes</a></li>
@@ -16,35 +17,35 @@
 						</ul>
 					</li>
 
-					<li><a href="#"><i class="fa fa-glass"></i>Events </a></li>
-					<li><a href="#"><i class="fa fa-file-image-o"></i>Gallery </a></li>
-					<li><a href="#"><i class="fa fa-cog"></i>Services </a>
+					<li>
+						<a href="#"><i class="fa fa-book"></i>Documentos</a>
 						<ul class="submenu">
-							<li><a href="#">Web Design</a></li>
-							<li><a href="#">Hosting</a></li>
-							<li><a href="#">Design</a>
-								<ul class="submenu">
-									<li><a href="#">Graphics </a></li>
-									<li><a href="#">Vectors </a></li>
-									<li><a href="#">Photoshop </a></li>
-									<li><a href="#">Fonts </a></li>
-								</ul>
-							</li>
-							<li><a href="#">Consulting </a></li>
+							<li><a href="regimento.php">Regimento</a></li>
+							<li><a href="#">Reconhecimento de diploma</a></li>
+							<li><a href="resolucoes.php">Resoluções</a></li>
+							<li><a href="formularios.php">Formulários</a></li>
+							<li><a href="editais.php">Editais</a></li>
+							<li><a href="#">Estrutura Curricular</a></li>
 						</ul>
 					</li>
-					<li><a href="#"><i class="fa fa-newspaper-o"></i>News </a></li>
-					<li><a href="#"><i class="fa fa-suitcase"></i>Portfolio </a>
+
+					<li>
+						<a href="#"><i class="fa fa-tags"></i>Seleção</a>
+					</li>
+					
+					<li>
+						<a href="#"><i class="fa fa-university"></i>Parcerias </a>
 						<ul class="submenu">
-							<li><a href="#">Web Design </a></li>
-							<li><a href="#">Graphics </a></li>
-							<li><a href="#">Photoshop </a></li>
-							<li><a href="#">Programming </a></li>
+							<li><a href="#">Intercâmbios</a></li>
+							<li><a href="#">Convênios Nacionais</a></li>
+							<li><a href="#">Convênios Internacionais</a></li>
 						</ul>
 					</li>
-					<li><a href="#"><i class="fa fa-user"></i>About </a></li>
-					<li><a href="#"><i class="fa fa-envelope"></i>Contact </a></li>
+
+					<li><a href="#"><i class="fa fa-newspaper-o"></i>Eventos </a></li>
+
+					<li><a href="#"><i class="fa fa-suitcase"></i>Auditórios </a></li>
 				</ul>
-				<div class="jquery-accordion-menu-footer">Footer </div>
+				<div class="jquery-accordion-menu-footer">Footer</div>
 			</div>
 		</div>
